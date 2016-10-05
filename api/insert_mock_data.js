fa81eccd946b9a12c68c8e8ba59b90e268f059f5
Rafa Rubio <rubio_rafmor@gva.es>
2016-10-05 13:58:02 +0200

    + New model WIP

--- api/insert_mock_data.js
+++ api/insert_mock_data.js
@@ -524,41 +524,246 @@ var course_2_15 = {
 
 // 2016 assessments and courses
 var assessment_session_1_1_16 = {
-    order: '1',
+    _id: '1_1_16',
     name: '1st assessment',
-    students_qualifications: [
-	{student_id: '1', qualifications: [6, 5, null, 8, 3, 4, null, 7, 5, 6, 8, 5]},
-	{student_id: '2', qualifications: [9, 8, 7, null, 7, 9, 5, 7, null, 6, 9, 8]},
-	{student_id: '3', qualifications: [10, 9, 10, null, 8, 10, 8, 10, null, 9, 8, 10]},
-	{student_id: '4', qualifications: [4, 4, null, 6, 4, 2, null, 5, 5, 4, 3, 5]},
-	{student_id: '5', qualifications: [7, 5, 5, null, 5, 8, null, 4, 5, 6, 6, 4]},
-	{student_id: '6', qualifications: [6, 3, null, 6, 5, 3, null, 6, 3, 4, 4, 5]},
+    students: [
+	{
+            student_id: '1',
+            repeater: false,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 5},
+                {subject_id: 'FQ',   qualification: 6},
+                {subject_id: 'GH',   qualification: null},
+                {subject_id: 'LCL',  qualification: 8},
+                {subject_id: 'AN',   qualification: 3},
+                {subject_id: 'MAC',  qualification: 4},
+                {subject_id: 'MAP',  qualification: null},
+                {subject_id: 'REL',  qualification: 7},
+                {subject_id: 'MUS',  qualification: 5},
+                {subject_id: 'EPVA', qualification: 6},
+                {subject_id: 'VLL',  qualification: 8},
+                {subject_id: 'INF',  qualification: 5}
+            ]
+        },
+	{
+            student_id: '7',
+            repeater: true,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 9},
+                {subject_id: 'FQ',   qualification: 8},
+                {subject_id: 'GH',   qualification: 7},
+                {subject_id: 'LCL',  qualification: null},
+                {subject_id: 'AN',   qualification: 7},
+                {subject_id: 'MAC',  qualification: 9},
+                {subject_id: 'MAP',  qualification: 5},
+                {subject_id: 'REL',  qualification: 7},
+                {subject_id: 'MUS',  qualification: null},
+                {subject_id: 'EPVA', qualification: 6},
+                {subject_id: 'VLL',  qualification: 9},
+                {subject_id: 'INF',  qualification: 8}
+            ]
+        },
+	{
+            student_id: '3',
+            repeater: false,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 10},
+                {subject_id: 'FQ',   qualification: 9},
+                {subject_id: 'GH',   qualification: 10},
+                {subject_id: 'LCL',  qualification: null},
+                {subject_id: 'AN',   qualification: 8},
+                {subject_id: 'MAC',  qualification: 10},
+                {subject_id: 'MAP',  qualification: 8},
+                {subject_id: 'REL',  qualification: 10},
+                {subject_id: 'MUS',  qualification: null},
+                {subject_id: 'EPVA', qualification: 9},
+                {subject_id: 'VLL',  qualification: 8},
+                {subject_id: 'INF',  qualification: 10}
+            ]
+        },
+	{
+            student_id: '9',
+            repeater: true,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 4},
+                {subject_id: 'FQ',   qualification: 4},
+                {subject_id: 'GH',   qualification: null},
+                {subject_id: 'LCL',  qualification: 6},
+                {subject_id: 'AN',   qualification: 4},
+                {subject_id: 'MAC',  qualification: 2},
+                {subject_id: 'MAP',  qualification: null},
+                {subject_id: 'REL',  qualification: 5},
+                {subject_id: 'MUS',  qualification: 5},
+                {subject_id: 'EPVA', qualification: 4},
+                {subject_id: 'VLL',  qualification: 3},
+                {subject_id: 'INF',  qualification: 5}
+            ]
+        },
+	{
+            student_id: '5',
+            repeater: false,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 7},
+                {subject_id: 'FQ',   qualification: 5},
+                {subject_id: 'GH',   qualification: 5},
+                {subject_id: 'LCL',  qualification: null},
+                {subject_id: 'AN',   qualification: 5},
+                {subject_id: 'MAC',  qualification: 8},
+                {subject_id: 'MAP',  qualification: null},
+                {subject_id: 'REL',  qualification: 4},
+                {subject_id: 'MUS',  qualification: 5},
+                {subject_id: 'EPVA', qualification: 6},
+                {subject_id: 'VLL',  qualification: 6},
+                {subject_id: 'INF',  qualification: 4}
+            ]
+        },
+	{
+            student_id: '11',
+            repeater: false,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 6},
+                {subject_id: 'FQ',   qualification: 3},
+                {subject_id: 'GH',   qualification: null},
+                {subject_id: 'LCL',  qualification: 6},
+                {subject_id: 'AN',   qualification: 5},
+                {subject_id: 'MAC',  qualification: 3},
+                {subject_id: 'MAP',  qualification: null},
+                {subject_id: 'REL',  qualification: 6},
+                {subject_id: 'MUS',  qualification: 3},
+                {subject_id: 'EPVA', qualification: 4},
+                {subject_id: 'VLL',  qualification: 4},
+                {subject_id: 'INF',  qualification: 5}
+            ]
+        }
     ]
 };
 
 var assessment_session_1_2_16 = {
-    order: '2',
+    _id: '1_2_16',
     name: '2nd assessment',
-    students_qualifications: [
-	{student_id: '7', qualifications: [8, 9, 7, 7, null, 8, 9, null, 7, 7, 8, 7]},
-	{student_id: '8', qualifications: [4, 4, null, 5, 3, 6, 4, null, 4, 5, 4, 5]},
-	{student_id: '9', qualifications: [5, 5, null, 5, 5, 5, null, 4, 6, 7, 7, 5]},
-	{student_id: '10', qualifications: [8, 7, 9, null, 8, 7, 5, 8, null, 8, 8, 8]},
-	{student_id: '11', qualifications: [9, 9, null, 7, 10, 10, 9, null, 9, 7, 10, 9]},
-	{student_id: '12', qualifications: [5, 4, null, 5, 5, 6, null, 4, 6, 5, 7, 4]}
+    students: [
+	{
+            student_id: '1',
+            repeater: false,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 8},
+                {subject_id: 'FQ',   qualification: 9},
+                {subject_id: 'GH',   qualification: 7},
+                {subject_id: 'LCL',  qualification: 7},
+                {subject_id: 'AN',   qualification: null},
+                {subject_id: 'MAC',  qualification: 8},
+                {subject_id: 'MAP',  qualification: 9},
+                {subject_id: 'REL',  qualification: null},
+                {subject_id: 'MUS',  qualification: 7},
+                {subject_id: 'EPVA', qualification: 7},
+                {subject_id: 'VLL',  qualification: 8},
+                {subject_id: 'INF',  qualification: 7}
+            ]
+        },
+	{
+            student_id: '7',
+            repeater: true,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 4},
+                {subject_id: 'FQ',   qualification: 4},
+                {subject_id: 'GH',   qualification: null},
+                {subject_id: 'LCL',  qualification: 5},
+                {subject_id: 'AN',   qualification: 3},
+                {subject_id: 'MAC',  qualification: 6},
+                {subject_id: 'MAP',  qualification: 4},
+                {subject_id: 'REL',  qualification: null},
+                {subject_id: 'MUS',  qualification: 4},
+                {subject_id: 'EPVA', qualification: 5},
+                {subject_id: 'VLL',  qualification: 4},
+                {subject_id: 'INF',  qualification: 5}
+            ]
+        },
+	{
+            student_id: '3',
+            repeater: false,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 5},
+                {subject_id: 'FQ',   qualification: 5},
+                {subject_id: 'GH',   qualification: null},
+                {subject_id: 'LCL',  qualification: 5},
+                {subject_id: 'AN',   qualification: 5},
+                {subject_id: 'MAC',  qualification: 5},
+                {subject_id: 'MAP',  qualification: null},
+                {subject_id: 'REL',  qualification: 4},
+                {subject_id: 'MUS',  qualification: 6},
+                {subject_id: 'EPVA', qualification: 7},
+                {subject_id: 'VLL',  qualification: 7},
+                {subject_id: 'INF',  qualification: 4}
+            ]
+        },
+	{
+            student_id: '9',
+            repeater: true,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 8},
+                {subject_id: 'FQ',   qualification: 7},
+                {subject_id: 'GH',   qualification: 9},
+                {subject_id: 'LCL',  qualification: null},
+                {subject_id: 'AN',   qualification: 8},
+                {subject_id: 'MAC',  qualification: 7},
+                {subject_id: 'MAP',  qualification: 5},
+                {subject_id: 'REL',  qualification: 8},
+                {subject_id: 'MUS',  qualification: null},
+                {subject_id: 'EPVA', qualification: 8},
+                {subject_id: 'VLL',  qualification: 8},
+                {subject_id: 'INF',  qualification: 8}
+            ]
+        },
+	{
+            student_id: '5',
+            repeater: false,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 9},
+                {subject_id: 'FQ',   qualification: 9},
+                {subject_id: 'GH',   qualification: null},
+                {subject_id: 'LCL',  qualification: 7},
+                {subject_id: 'AN',   qualification: 10},
+                {subject_id: 'MAC',  qualification: 10},
+                {subject_id: 'MAP',  qualification: 9},
+                {subject_id: 'REL',  qualification: null},
+                {subject_id: 'MUS',  qualification: 9},
+                {subject_id: 'EPVA', qualification: 7},
+                {subject_id: 'VLL',  qualification: 10},
+                {subject_id: 'INF',  qualification: 9}
+            ]
+        },
+	{
+            student_id: '11',
+            repeater: ,
+            qualifications: [
+                {subject_id: 'BG',   qualification: 5},
+                {subject_id: 'FQ',   qualification: 4},
+                {subject_id: 'GH',   qualification: null},
+                {subject_id: 'LCL',  qualification: 5},
+                {subject_id: 'AN',   qualification: 5},
+                {subject_id: 'MAC',  qualification: 6},
+                {subject_id: 'MAP',  qualification: null},
+                {subject_id: 'REL',  qualification: 4},
+                {subject_id: 'MUS',  qualification: 6},
+                {subject_id: 'EPVA', qualification: 5},
+                {subject_id: 'VLL',  qualification: 7},
+                {subject_id: 'INF',  qualification: 4}
+            ]
+        }
     ]
 };
 
 var course_1_16 = {
+    school_id: 'AS',
     start_year: 2016,
     academic_year: '2016-17',
     stage: 'ESO',
     level: '3',
     name: 'C',
-    repeaters: ['1', '4'],
-    subjects_id: ['BG', 'FQ', 'GH', 'LCL', 'AN', 'MAC', 'MAP', 'REL', 'MUS', 'EPVA', 'VLL', 'INF'],
-    subjects_name: ['Biologia', 'Física i Química', 'Geografia', 'Llengua Castellana i Literatura', 'Anglès', 'Matemàtiques Científiques', 'Matemàtiques Aplicades', 'Religió', 'Música', 'Educació Plàstica i Visual', 'Valencià Llengua i Literatura', 'Informàtica'],
-    assessment_sessions: [assessment_session_1_1_16, assessment_session_1_2_16]
+    assessments: [
+        {assessment_id: '1_1_16', order: 1},
+        {assessment_id: '1_2_16', order: 2}
+    ]
 };
 
 var assessment_session_2_1_16 = {
