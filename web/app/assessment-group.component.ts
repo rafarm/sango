03238692d077e0071d1a59490f3562af6c796ab8
Rafa Rubio <rubio_rafmor@gva.es>
2016-10-19 07:28:34 +0200

    + New ratios chart added to group view.

--- web/app/assessment-group.component.ts
+++ web/app/assessment-group.component.ts
@@ -30,6 +30,8 @@ export class AssessmentGroupComponent implements OnChanges {
     histogram_ChartOptions: {};
     averages_ChartData = [];
     averages_ChartOptions: {};
+    ratios_ChartData = [];
+    ratios_ChartOptions: {};
 
     constructor() {
 	this.pie_ChartOptions = {
@@ -47,7 +49,7 @@ export class AssessmentGroupComponent implements OnChanges {
 	    }
 	}
 	this.histogram_ChartOptions = {
-	    title: 'Histograma d\'assignatures aprovades',
+	    title: 'Assignatures aprovades',
 	    titleTextStyle: {
 		fontSize: 18
 	    },
@@ -79,7 +81,7 @@ export class AssessmentGroupComponent implements OnChanges {
 		alignment: 'end'
 	    },
 	    chartArea: {
-		width: '94%'
+		width: '90%'
 	    },
 	    vAxis: {
 		gridlines: {
@@ -95,6 +97,32 @@ export class AssessmentGroupComponent implements OnChanges {
 		}
 	    }
 	}
+	this.ratios_ChartOptions = {
+            title: 'Ratio aprovats per assignatura',
+            titleTextStyle: {
+                fontSize: 18
+            },
+            legend: {
+                position: 'top',
+                alignment: 'end'
+            },
+            chartArea: {
+                width: '90%'
+            },
+            vAxis: {
+                gridlines: {
+                    count: 3
+                },
+                maxValue: 1,
+                minValue: 0
+            },
+            seriesType: 'bars',
+            series: {
+                0: {
+                    type: 'line'
+                }
+            }
+        }
     }
 
     ngOnChanges(changes: SimpleChanges) {
@@ -102,67 +130,114 @@ export class AssessmentGroupComponent implements OnChanges {
 	this.pie_ChartData = [];
 	this.histogram_ChartData = [];
 	this.averages_ChartData = [];
+	this.ratios_ChartData = [];
 
-	// Set pie data
+	this.setPieData();
+	this.setHistogramData();
+	this.setAveragesData();
+	this.setRatiosData();    
+    }
+
+    private setPieData() {
 	let pie_data = this.pie_ChartData;
-	pie_data.push( ['#Suspeses', '#Alumnes'] );
-	pie_data.push( ['Cap', 0] );
-	pie_data.push( ['1', 0] );
-	pie_data.push( ['2', 0] );
-	pie_data.push( ['3', 0] );
-	pie_data.push( ['4+', 0] );
-	//for (var i = 0; i < studentsStats.length; i++) {
-	for (let i in this.studentStats) {
-	    let num_failed = this.studentStats[i].failed;
-	    if (num_failed <= 3) {
-		pie_data[num_failed+1][1]++;
-	    }
-	    else {
-		pie_data[5][1]++;
-	    }
-	}
-
-	// Set histogram data
-	let hist_data = this.histogram_ChartData;
-	hist_data.push( ['Alumne', 'Aprovades'] );
-	//for (var i = 0; i < students.length; i++) {
-	for (let i in this.studentStats) {
-	    let student = this.students[i];
-	    hist_data.push( [student.last_name + ', ' + student.first_name, this.studentStats[i].passed] );
-	}
+        pie_data.push( ['#Suspeses', '#Alumnes'] );
+        pie_data.push( ['Cap', 0] );
+        pie_data.push( ['1', 0] );
+        pie_data.push( ['2', 0] );
+        pie_data.push( ['3', 0] );
+        pie_data.push( ['4+', 0] );
         
-	// Set averages by subject data
+	for (let i in this.studentStats) {
+            let num_failed = this.studentStats[i].failed;
+            if (num_failed <= 3) {
+                pie_data[num_failed+1][1]++;
+            }
+            else {
+                pie_data[5][1]++;
+            }
+        }
+    }
+
+    private setHistogramData() {
+	let hist_data = this.histogram_ChartData;
+        hist_data.push( ['Alumne', 'Aprovades'] );
+        
+	for (let i in this.studentStats) {
+            let student = this.students[i];
+            hist_data.push( [student.last_name + ', ' + student.first_name,
+			    this.studentStats[i].passed] );
+        }
+    }
+
+    private setAveragesData() {
 	let averages_data = this.averages_ChartData;
-	let stats = this.subjectStats;
-	// Fins selected assessment order
+        let stats = this.subjectStats;
+	let order = this.findAssessmentOrder();        
+
+        // ...Headers
+        let headers = ['Assignatura', 'Mitjana nivell'];
+        for (var i = 0; i <= order; i++) {
+            headers.push( this.course.assessments[i].name );
+        }
+        averages_data.push( headers );
+
+        // ...Values
+        for (let s in stats[this.course.assessments[0].assessment_id].stats) {
+            // Subject id
+            let values = [s];
+
+            // Level avg for subject
+            values.push( this.levelStats[s].avg );
+
+            // Assessment avg for subject
+            for (var i=0; i<=order; i++) {
+                let a_id = this.course.assessments[i].assessment_id;
+                values.push( this.subjectStats[a_id].stats[s].avg );
+            }
+
+            averages_data.push( values );
+        }
+    }
+
+    private setRatiosData() {
+        let ratios_data = this.ratios_ChartData;
+        let stats = this.subjectStats; 
+        let order = this.findAssessmentOrder();
+
+        // ...Headers
+        let headers = ['Assignatura', 'Ratio nivell'];
+        for (var i = 0; i <= order; i++) {
+            headers.push( this.course.assessments[i].name );
+        }
+        ratios_data.push( headers );
+
+        // ...Values
+        for (let s in stats[this.course.assessments[0].assessment_id].stats) {
+            // Subject id
+            let values = [s];
+
+            // Level ratio for subject
+            values.push( this.levelStats[s].ratio );
+
+            // Assessment ratio for subject
+            for (var i=0; i<=order; i++) {
+                let a_id = this.course.assessments[i].assessment_id;
+                values.push( this.subjectStats[a_id].stats[s].ratio );
+            }
+
+            ratios_data.push( values );
+        }
+    }
+
+    private findAssessmentOrder(): number {
 	var order = 0;
-	while (order<this.course.assessments.length && 
-		this.course.assessments[order].assessment_id !== this.assessmentId) {
-	    order++;
-	}
-	console.info('Setting stats charts data: ' + order);
-	// ...Headers
-	let headers = ['Assignatura', 'Mitjana nivell'];
-	for (var i = 0; i <= order; i++) {
-	    headers.push( this.course.assessments[i].name );
-	}
-	averages_data.push( headers );
-	// ...Values
-	for (let s in stats[this.course.assessments[0].assessment_id].stats) {
-	    // Subject id
-	    let values = [s];
 
-	    // Level avg for subject
-	    values.push( this.levelStats[s].avg );
+        while (order<this.course.assessments.length && 
+                this.course.assessments[order].assessment_id !== this.assessmentId) {
+            order++;
+        }
 
-	    // Assessment avg for subject
-	    for (var i=0; i<=order; i++) {
-		let a_id = this.course.assessments[i].assessment_id;
-		values.push( this.subjectStats[a_id].stats[s].avg );
-	    }
-
-	    averages_data.push( values );
-	}
+	return order;
     }
 }
 
