64d2570bde7d9b8a89a36d7e7fa906c3ee07ab67
Rafa Rubio <rubio_rafmor@gva.es>
2016-10-27 07:09:21 +0200

    + Itaca parser WIP.

--- api/parsers/itaca.js
+++ api/parsers/itaca.js
@@ -4,15 +4,13 @@ var xmldoc = require('xmldoc');
 function parser(req, res) {
     console.log('Ingest - parse init: '+ req.file.originalname);
     if (req.file.mimetype != 'text/xml') {
-	res.status(500);
-	res.json({msg:'Not an xml file.'});
+	responseError('Not an xml file.', res);
 	return;
     }
     
     var doc = new xmldoc.XmlDocument(req.file.buffer);
     if (doc.name != 'centro') {
-	res.status(500);
-        res.json({msg:'Invalid file format.'});
+        responseError('Invalid file format.', res);
 	return;
     }
 
@@ -22,12 +20,69 @@ function parser(req, res) {
 	{ $set: { name: doc.attr.denominacion } },
 	{ upsert: true })
 	.then(function(result) {
-	    res.json(result);
+	    parseDoc(doc, 0, {}, res);
 	})
 	.catch(function(error) {
 	    res.status(500);
 	    res.json(error);	    
 	});
+}
+
+function responseError(msg, res) {
+    res.status(500);
+    res.json({msg: msg});
+}
+
+function parseDoc(doc, index, result, res) {
+    if (index < doc.children.length) {
+	var child = doc.children[index];
+
+	switch (child.name) {
+	    case 'contenidos':
+		parseSubjects(child, doc, index, result, res);
+		break;
+	    default: // Continue...
+		parseDoc(doc, ++index, result, res);
+	}
+    }
+
+    res.json(result);
+}
+
+function parseSubjects(subjects, doc, index, result, res) {
+    var operations = [];
+
+    for (var s=0; s<subjects.children.length; s++) {
+	var sub = subjects.children[s];
+
+	if (sub.name != 'contenido') {
+	    responseError('Invalid file format.', res);
+	    return;
+	}
+
+	var op = {
+	    updateOne: {
+		filter: { _id: sub.attr.codigo },
+		update: { $set: {
+		    level: sub.attr.ensenanza,
+		    name: sub.attr.nombre_val
+		} },
+		upsert: true 
+	    }
+	};
+
+	operations.push(op);
+    }
+
+     mongodb.db.collection('subjects').bulkWrite(operations)
+	.then(function(resp) {
+	    result['subjects'] = resp;
+            parseDoc(doc, ++index, result, res);
+        })
+        .catch(function(error) {
+            res.status(500);
+            res.json(error);
+        });
 }
 
 module.exports = parser;
