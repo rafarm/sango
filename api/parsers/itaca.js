da2be5fa76211492fa7ab1153baf4c7fafef8ae4
Rafa Rubio <rubio_rafmor@gva.es>
2016-10-27 09:41:19 +0200

    + Subjects&Students ingest completed.

--- api/parsers/itaca.js
+++ api/parsers/itaca.js
@@ -41,12 +41,16 @@ function parseDoc(doc, index, result, res) {
 	    case 'contenidos':
 		parseSubjects(child, doc, index, result, res);
 		break;
+	    case 'alumnos':
+		parseStudents(child, doc, index, result, res);
+		break;
 	    default: // Continue...
 		parseDoc(doc, ++index, result, res);
 	}
     }
-
-    res.json(result);
+    else {
+        res.json(result);
+    }
 }
 
 function parseSubjects(subjects, doc, index, result, res) {
@@ -55,28 +59,62 @@ function parseSubjects(subjects, doc, index, result, res) {
     for (var s=0; s<subjects.children.length; s++) {
 	var sub = subjects.children[s];
 
-	if (sub.name != 'contenido') {
-	    responseError('Invalid file format.', res);
-	    return;
+	if (sub.name == 'contenido') {
+	    var op = {
+	        updateOne: {
+		    filter: { _id: sub.attr.codigo },
+		    update: { $set: {
+		        level: sub.attr.ensenanza,
+		        name: sub.attr.nombre_val,
+		        school_id: doc.attr.codigo
+		    } },
+		    upsert: true 
+	    	}
+	    };
+
+	    operations.push(op);
 	}
-
-	var op = {
-	    updateOne: {
-		filter: { _id: sub.attr.codigo },
-		update: { $set: {
-		    level: sub.attr.ensenanza,
-		    name: sub.attr.nombre_val
-		} },
-		upsert: true 
-	    }
-	};
-
-	operations.push(op);
     }
 
-     mongodb.db.collection('subjects').bulkWrite(operations)
+    mongodb.db.collection('subjects').bulkWrite(operations)
 	.then(function(resp) {
-	    result['subjects'] = resp;
+	    result['subjects'] = resp.nUpserted;
+            parseDoc(doc, ++index, result, res);
+        })
+        .catch(function(error) {
+            res.status(500);
+            res.json(error);
+        });
+}
+
+function parseStudents(students, doc, index, result, res) {
+    var operations = [];
+
+    for (var s=0; s<students.children.length; s++) {
+        var st = students.children[s];
+
+        if (st.name == 'alumno') {
+            var op = {
+                updateOne: {
+                    filter: { _id: st.attr.NIA },
+                    update: { $set: {
+                        last_name: st.attr.apellido1 + ' ' + st.attr.apellido2,
+                        first_name: st.attr.nombre,
+			birth_date: st.attr.fecha_nac,
+			genre: st.attr.sexo,
+                        school_id: doc.attr.codigo
+                    } },
+                    upsert: true
+                }
+            };
+
+            operations.push(op);
+        }
+    }
+
+    mongodb.db.collection('students').bulkWrite(operations)
+        .then(function(resp) {
+            result['students'] = resp.nUpserted;
             parseDoc(doc, ++index, result, res);
         })
         .catch(function(error) {
