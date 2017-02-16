85c58a083125340e09cfbeb0a2173990947bf11a
Rafa Rubio <rubio_rafmor@gva.es>
2017-02-16 09:56:21 +0100

    Itaca parser WIP

--- api/parsers/itaca.js
+++ api/parsers/itaca.js
@@ -44,7 +44,7 @@ function parser(req, res) {
 }
 
 // Use recursion to parse data file...
-function parseDoc(doc, childName,  res) {
+function parseDoc(doc, childName, res) {
     if (childName != null) {
     	var child = doc.childNamed(childName);
     
@@ -52,7 +52,7 @@ function parseDoc(doc, childName,  res) {
 	    switch (child.name) {
                 case 'ensenanzas':
 		    res.sseSend('Processing stages...');
-                    parseStages(child, doc, res);
+                    parseChildren(child, processStage, 'stages', null, doc, res);
                     break;
                 default: // Error...
                     res.sseError('"' + childName + '" entity data not processed.');
@@ -67,49 +67,30 @@ function parseDoc(doc, childName,  res) {
     }
 }
 
-function parseStages(item, doc, res) {
+function parseChildren(item, process, collection, next, doc, res) {
     var operations = [];
 
     for (var i=0; i<item.children.length; i++) {
         var child = item.children[i];
 
-	var op = processStage(child);
+	var op = process(child);
 	if (op != null) {
 	    operations.push(op);
 	}
     }
 
     if (operations.length > 0) {
-	mongodb.db.collection('stages').bulkWrite(operations)
+	mongodb.db.collection(collection).bulkWrite(operations)
 	    .then(resp => {
-		parseDoc(doc, null, res);
+		parseDoc(doc, next, res);
 	    })
 	    .catch(error => {
 		res.sseError(error);
 	    });
     }
     else {
-	res.sseError('No stages found in data file');
+	res.sseError('No ' + collection + ' found in data file');
     }
-}
-
-function parseChildren(item, process, collection, doc, res) {
-    var operations = [];
-
-    for (var i=0; i<item.children.length; i++) {
-        var c = item.children[i];
-	operations.push(process(c, doc));
-    }
-
-    mongodb.db.collection(collection).bulkWrite(operations)
-        .then(function(resp) {
-            //result[collection] = resp.nInserted + resp.nUpserted + resp.nMatched;
-            parseDoc(doc, ++index, result, res);
-        })
-        .catch(function(error) {
-            res.status(500);
-            res.json(error);
-        });
 }
 
 function processStage(child) {
@@ -172,77 +153,5 @@ function processStudent(st, doc) {
 
     return null;
 }
-
-/*
-function parseSubjects(subjects, doc, index, result, res) {
-    var operations = [];
-
-    for (var s=0; s<subjects.children.length; s++) {
-	var sub = subjects.children[s];
-
-	if (sub.name == 'contenido') {
-	    var op = {
-	        updateOne: {
-		    filter: { _id: sub.attr.codigo },
-		    update: { $set: {
-		        level: sub.attr.ensenanza,
-		        name: sub.attr.nombre_val,
-		        school_id: doc.attr.codigo
-		    } },
-		    upsert: true 
-	    	}
-	    };
-
-	    operations.push(op);
-	}
-    }
-
-    mongodb.db.collection('subjects').bulkWrite(operations)
-	.then(function(resp) {
-	    result['subjects'] = resp.nUpserted;
-            parseDoc(doc, ++index, result, res);
-        })
-        .catch(function(error) {
-            res.status(500);
-            res.json(error);
-        });
-}
-
-function parseStudents(students, doc, index, result, res) {
-    var operations = [];
-
-    for (var s=0; s<students.children.length; s++) {
-        var st = students.children[s];
-
-        if (st.name == 'alumno') {
-            var op = {
-                updateOne: {
-                    filter: { _id: st.attr.NIA },
-                    update: { $set: {
-                        last_name: st.attr.apellido1 + ' ' + st.attr.apellido2,
-                        first_name: st.attr.nombre,
-			birth_date: st.attr.fecha_nac,
-			genre: st.attr.sexo,
-                        school_id: doc.attr.codigo
-                    } },
-                    upsert: true
-                }
-            };
-
-            operations.push(op);
-        }
-    }
-
-    mongodb.db.collection('students').bulkWrite(operations)
-        .then(function(resp) {
-            result['students'] = resp.nUpserted;
-            parseDoc(doc, ++index, result, res);
-        })
-        .catch(function(error) {
-            res.status(500);
-            res.json(error);
-        });
-}
-*/
 
 module.exports = parser;
