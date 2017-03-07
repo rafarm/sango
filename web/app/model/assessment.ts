79c7ad019ad6f3f28402544e4ba1d1bf7a8b6b74
Rafa Rubio <rubio_rafmor@gva.es>
2017-03-07 06:55:29 +0100

    Assessment qualifications model added.

--- web/app/model/assessment.ts
+++ web/app/model/assessment.ts
@@ -1,16 +1,25 @@
 class Qualification {
     subject_id: string;
+    enroled: boolean;
+    adapted: boolean;
     qualification: number;
 }
 
 class Student {
-    student_id: string;
-    repeater: boolean;
+    _id: string;
+    first_name: string;
+    last_name: string;
     qualifications: Qualification[];
+}
+
+class Subject {
+    _id: string;
+    name: string;
 }
 
 export class Assessment {
     _id: string;
     name: string;
+    subjects: Subject[];
     students: Student[];
 }
