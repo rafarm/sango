32fa26cbafe2337e3aa7041fcccf64b073c0e140
Rafa Rubio <rubio_rafmor@gva.es>
2017-04-16 22:34:09 +0200

    Divider now in utils module.

---
+++ web/app/utils/nav-divider.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } 	from '@angular/core';
+import { Observable }		from 'rxjs/Observable';
+
+export class NavItem {
+    constructor(
+	public _id: string,
+	public name: string
+    ) {}
+}
+
+@Component({
+    moduleId: module.id,
+    selector: 'nav-divider', 
+    templateUrl: './nav-divider.component.html'
+})
+export class NavDividerComponent {
+    @Input()
+    items: Observable<NavItem[]>;
+}
