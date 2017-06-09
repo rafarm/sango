52294ba5695e160cb1276d69897e7597011e77d7
Rafa Rubio <rubio_rafmor@gva.es>
2017-06-09 13:03:10 +0200

    Students list WIP.

---
+++ web/app/assessments/students/students-list.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, OnDestroy } 	from '@angular/core';
+import { Router, ActivatedRoute, Params }       from '@angular/router';
+import { Observable }                           from 'rxjs/Observable';
+import { Subscription }                         from 'rxjs/Subscription';
+
+import { AssessmentsService }			from '../assessments.service';
+
+import { Group }                        	from '../../model/group';
+//import { Course } from '../../model/course';
+//import { Student } from './model/student';
+//import { AssessmentStats } from './model/assessment-stats';
+    
+@Component({
+    moduleId: module.id,
+    templateUrl: './students-list.component.html'
+})
+export class StudentsListComponent implements OnInit, OnDestroy {
+    year: string;
+    group_id: string;
+    assessment_id: string;
+    students: any;
+    subjects: any;
+    studentStats: any;
+    subjectStats: any;
+    levelStats: any;
+
+    private statsSubscription: Subscription;
+
+
+    constructor(
+	private assessmentsService: AssessmentsService,
+	private route: ActivatedRoute,
+	private router: Router
+    ) {}
+
+    ngOnInit() {
+	this.year = this.route.parent.parent.parent.snapshot.params['year'];
+        this.group_id = this.route.parent.parent.parent.snapshot.params['group_id'];
+	this.statsSubscription = this.route.params.subscribe((params: Params) => {
+	    this.assessment_id = params['assessment_id'];
+
+	    // Get students and subjects...
+	    this.assessmentsService.getGroup(this.group_id)
+		.subscribe((group: Group) => {
+		    this.students = group.students;
+		    this.subjects = group.subjects;
+		});
+
+	    // Get students' stats...
+	    this.assessmentsService.getStudentStats(this.assessment_id, this.group_id).subscribe((stats: any) => this.studentStats = stats);
+	    
+	    // Get subjects' stats...
+	    this.assessmentsService.getSubjectStats(this.assessment_id, this.group_id).subscribe((stats: any) => this.subjectStats = stats);
+	    
+	    // Get subjects' level stats...
+	    this.assessmentsService.getSubjectStats(this.assessment_id).subscribe((stats: any) => this.levelStats = stats);
+	});
+    }
+
+    ngOnDestroy() {
+        this.statsSubscription.unsubscribe();
+    }
+}
+
