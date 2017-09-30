f6dfa40796f912b71cacf351f34a7fb00c7528ae
Rafa Rubio <rubio_rafmor@gva.es>
2017-09-30 18:31:16 +0200

    Assessments students list component layout works.

--- web/app/utils/stretchable.component.ts
+++ web/app/utils/stretchable.component.ts
@@ -23,17 +23,19 @@ export class StretchableComponent implements AfterContentChecked {
     private stretchElements() {
 	let contentElem = document.getElementById('content');
 	let windowHeight = window.innerHeight;
-        let contentHeight = contentElem.offsetHeight;
-        if (windowHeight == contentHeight) {
+        if (windowHeight == contentElem.offsetHeight) {
             return;
         }
 
 	for (var i=0; i<this.stretchables.length; i++) {
 	    let element = <HTMLElement>this.stretchables.item(i);
-	    console.log(element.style);
+	    let elementPosition = window.getComputedStyle(element).position;
 	    let newHeight = 0;
-	    if (window.getComputedStyle(element).position == 'absolute') {
-		newHeight = windowHeight - element.getBoundingClientRect().top;
+
+	    if (elementPosition === 'absolute' || elementPosition === 'fixed') {
+		if (element.classList.contains('sidebar')) {
+		    newHeight = windowHeight - element.getBoundingClientRect().top - parseInt(window.getComputedStyle(contentElem).paddingBottom);
+		}
             }
 	    else {
 	    	element.style.height = windowHeight + 'px';
@@ -42,37 +44,4 @@ export class StretchableComponent implements AfterContentChecked {
 	    element.style.height = newHeight + 'px';
 	}
     }
-
-    /*
-    private stretchElements(resizing: boolean = false) {
-	let cElement = document.getElementById('content');
-        let wH = window.innerHeight;
-	let origCH = cElement.offsetHeight;
-	if (wH == origCH) {
-	    return;
-	}
-	
-	let debug = {}
-	debug['window'] = wH;
-
-	// For every stretchable element compute its new height
-        for (var i=0; i<this.stretchables.length; i++) {
-		let cH = cElement.offsetHeight;
-		if (cH > origCH && resizing) {
-		    cH = origCH;
-		}
-                let el = <HTMLElement>this.stretchables.item(i);
-		let element = {};
-		element['content'] = cH;
-		element['offset'] = el.offsetHeight
-		let top = el.getBoundingClientRect().top;
-		element['top'] = top;
-		let newH = el.offsetHeight + wH - cH;
-		element['newHeight'] = newH;
-                el.style.height = newH + 'px';
-		debug['element'+i] = element;
-        }
-	//console.log(debug);
-    }
-    */
 }
