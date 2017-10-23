8f58d109cb7307d311bead2538a07168c8b9094c
Rafa Rubio <rubio_rafmor@gva.es>
2017-10-23 12:12:01 +0200

    Assessments student detail view: Student card layout updated.

--- web/app/assessments/students/student-failed-chart.component.ts
+++ web/app/assessments/students/student-failed-chart.component.ts
@@ -9,7 +9,7 @@ import { Stats }		from '../../model/stats';
     moduleId: module.id,
     selector: 'failedChart',
     template: `
-	<div *ngIf="stats" id="failed_chart" chartType="ColumnChart" [chartData]="chartData" [chartOptions]="chartOptions" GoogleChart class="chart_400"></div>
+	<div *ngIf="stats" id="failed_chart" chartType="ColumnChart" [chartData]="chartData" [chartOptions]="chartOptions" GoogleChart style="height: 260px;"></div>
     `
 })
 export class StudentFailedChartComponent implements OnChanges {
