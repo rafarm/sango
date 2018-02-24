06ed8d11f714d3d5a8d0d1e0067c4f3c823749bb
Rafa Rubio <rubio_rafmor@gva.es>
2018-02-24 07:45:31 +0100

    Now session is stored in sango db.

--- api/session.js
+++ api/session.js
@@ -1,6 +1,9 @@
 var session = require('express-session');
 var MongoStore = require('connect-mongodb-session')(session);
 
+const db_sessions_uri = process.env.npm_package_config_db_url + 
+    '/' +
+    process.env.npm_package_config_db_name;
 const db_sessions_collection = 'sessions';
 
 module.exports = session({
@@ -9,7 +12,7 @@ module.exports = session({
 	    maxAge: parseInt(process.env.npm_package_config_cookie_max_age)
 	},
 	store: new MongoStore({
-	    uri: process.env.npm_package_config_db_url,
+	    uri: db_sessions_uri,
 	    collection: db_sessions_collection
 	}),
 	resave: false,
